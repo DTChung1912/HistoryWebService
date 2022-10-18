139c034b34668603a87d6ed954242966c687788e
chung <dtchung2k@gmail.com>
2022-10-18 11:43:03 +0700

    first commit

---
+++ src/main/kotlin/com/example/plugins/Templating.kt
@@ -0,0 +1,38 @@
+package com.example.plugins
+
+import freemarker.cache.*
+import io.ktor.server.freemarker.*
+import io.ktor.server.routing.*
+import io.ktor.server.html.*
+import kotlinx.html.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+import io.ktor.server.routing.*
+
+fun Application.configureTemplating() {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+    }
+
+
+    routing {
+        get("/html-freemarker") {
+            call.respond(FreeMarkerContent("index.ftl", mapOf("data" to IndexData(listOf(1, 2, 3))), ""))
+        }
+        get("/html-dsl") {
+            call.respondHtml {
+                body {
+                    h1 { +"HTML" }
+                    ul {
+                        for (n in 1..10) {
+                            li { +"$n" }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+data class IndexData(val items: List<Int>)
